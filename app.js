a843d558b91e9fa9afcf6ff94797b3f83e1ce835
AllisonLin8 <doging8@gmail.com>
2023-02-21 19:22:32 +0800

    Add delete function

--- app.js
+++ app.js
@@ -89,6 +89,14 @@ app.post('/restaurants/:restaurant_id/edit', (req, res) => {
         .then(restaurant => res.redirect(`/restaurants/${id}`))
         .catch(error => console.error(error))
 })
+// 刪除餐廳
+app.post('/restaurants/:restaurant_id/delete', (req, res) => {
+    const id = req.params.restaurant_id
+    return Restaurant.findById(id)
+        .then(restaurant => restaurant.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.error(error))
+})
 // 搜尋餐廳
 app.get('/search', (req, res) => {
     const keyword = req.query.keyword
